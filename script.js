449b6a4f78a4bc80a3eca3897870453b981f4ee3
p9844bz255-ship-it <p9844bz255@privaterelay.appleid.com>
2026-05-18 14:55:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -969,15 +969,15 @@ function cetakPDF(tipeJadwal) {
                 }
                 .kelas-info { 
                     font-size: 8px !important; 
-                    line-height: 1.5 !important; 
+                    line-height: 2.5 !important; 
                 }
                 .mapel { 
                     font-size: 8.5px !important; 
-                    line-height: 1.5 !important; 
+                    line-height: 2.5 !important; 
                 }
                 .guru-nama { 
                     font-size: 7.5px !important; 
-                    line-height: 1.5 !important; 
+                    line-height: 2.5 !important; 
                 }
 
                 /* CUSTOM FOOTNOTES */
